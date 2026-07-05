07165bf0dc545e426d0b9b0c7c0ab3548d8143d9
copymary9170 <copymary9170@gmail.com>
2026-07-05 19:21:53 -0400

    feat: activate historical commission snapshots

--- src/app_shell_payments.py
+++ src/app_shell_payments.py
@@ -3,6 +3,7 @@
 from src import app_shell
 from src.app_shell_goals import run_app as _run_base_app
 from src.cash_closing_reopen import activate_closing_reopen_support, render_cash_closing_reopen
+from src.commission_snapshots import activate_commission_snapshots
 from src.deletion_protection import activate_deletion_protection
 from src.financial_reconciliation import render_financial_reconciliation
 from src.payment_consistency import activate_payment_consistency
@@ -14,6 +15,7 @@ activate_payment_consistency()
 activate_deletion_protection()
 activate_closing_reopen_support()
 activate_restore_rollback()
+activate_commission_snapshots()
 app_shell.FUNCTIONAL_MODULES["Reversos de pagos"] = render_payment_reversals
 app_shell.FUNCTIONAL_MODULES["Conciliación financiera"] = render_financial_reconciliation
 app_shell.FUNCTIONAL_MODULES["Reabrir cierre de caja"] = render_cash_closing_reopen
