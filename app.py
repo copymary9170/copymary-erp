ea6472f94bf231d14cc4b6e9f49571ea4092ca41
copymary9170 <copymary9170@gmail.com>
2026-07-05 21:24:27 -0400

    activate daily control center

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
 """Punto de entrada de CopyMary ERP."""
+from src import app_shell
 from src.app_shell_payments import run_app
+from src.control_center_today import render_control_center_today
+
+app_shell.FUNCTIONAL_MODULES["Centro de control"] = render_control_center_today
 run_app()
