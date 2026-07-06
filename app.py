50c39f9cbfbf16756da563633cad33d44eb0d121
copymary9170 <copymary9170@gmail.com>
2026-07-06 14:28:19 -0400

    register supplier module

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from src.data_audit_insights import render_data_audit_insights
 from src.financial_dashboard_plus import render_financial_dashboard_plus
 from src.receipts_control import render_receipts_control
 from src import accounts_receivable_plus_loader
+from src import suppliers_intelligence_loader
 
 app_shell.FUNCTIONAL_MODULES["Centro de control"] = render_control_center_today
 app_shell.FUNCTIONAL_MODULES["Auditoría de datos"] = render_data_audit_insights
