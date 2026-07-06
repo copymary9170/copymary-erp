841a13be0a3566be4ab1d3d5df6b334bc3a4fb80
copymary9170 <copymary9170@gmail.com>
2026-07-06 12:20:06 -0400

    activate receivables risk module

--- src/accounts_receivable_plus_loader.py
+++ src/accounts_receivable_plus_loader.py
@@ -1,6 +1,6 @@
 """Registro del módulo ampliado de cuentas por cobrar."""
 
 from src import app_shell
-from src.accounts_receivable_plus import render_accounts_receivable_plus
+from src.accounts_receivable_risk import render_accounts_receivable_risk
 
-app_shell.FUNCTIONAL_MODULES["Cuentas por cobrar"] = render_accounts_receivable_plus
+app_shell.FUNCTIONAL_MODULES["Cuentas por cobrar"] = render_accounts_receivable_risk
