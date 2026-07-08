5339bdb8d7e1291d266957938aef44e465f14be3
copymary9170 <copymary9170@gmail.com>
2026-07-07 23:56:03 -0400

    resolve app entrypoint conflict

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ from src.stock_alerts_intelligence import render_stock_alerts_intelligence
 from src.costing_control import render_costing_control
 from src.price_adjustment_governance import render_price_adjustment_governance
 from src.price_io_governance import render_price_io_governance
-from src.cash_plus import render_cash_plus
+from src.cash_governance import render_cash_governance
 from src.financial_reconciliation import render_financial_reconciliation
 
 app_shell.FUNCTIONAL_MODULES["Centro de control"] = render_control_center_today
@@ -31,7 +31,6 @@ app_shell.FUNCTIONAL_MODULES["Alertas de inventario"] = render_stock_alerts_inte
 app_shell.FUNCTIONAL_MODULES["Costeo"] = render_costing_control
 app_shell.FUNCTIONAL_MODULES["Ajustar precios"] = render_price_adjustment_governance
 app_shell.FUNCTIONAL_MODULES["Exportar precios"] = render_price_io_governance
-app_shell.FUNCTIONAL_MODULES["Caja"] = render_cash_plus
+app_shell.FUNCTIONAL_MODULES["Caja"] = render_cash_governance
 app_shell.FUNCTIONAL_MODULES["Conciliación financiera"] = render_financial_reconciliation
-app_shell.NAVIGATION_GROUPS["Administración"] = (*app_shell.NAVIGATION_GROUPS["Administración"], "Conciliación financiera")
 run_app()
