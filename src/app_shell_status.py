0ef2fefccac5065d3e003597e8e47f85ced49bb6
copymary9170 <copymary9170@gmail.com>
2026-07-12 08:57:29 -0400

    chore: eliminar src/app_shell_status.py (código muerto, nunca se importaba; su lógica ya vive en module_bootstrap.py)

--- src/app_shell_status.py
+++
@@ -1,6 +0,0 @@
-from src.app_shell_payments import run_app as base_run
-from src.status_consistency import normalize_session_statuses
-
-def run_app():
-    normalize_session_statuses()
-    base_run()
