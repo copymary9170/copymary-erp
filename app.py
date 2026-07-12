351fc69ce41a3889e723e21c9525a99fe6066c85
copymary9170 <copymary9170@gmail.com>
2026-07-12 14:35:14 -0400

    Activar módulo de análisis de impresión

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 """Punto de entrada de CopyMary ERP."""
 from src.module_bootstrap import activate_module_bootstrap
+from src.print_cost_loader import activate_print_cost_module
 from src.top_navigation_app import run_app
 
 activate_module_bootstrap()
+activate_print_cost_module()
 run_app()
