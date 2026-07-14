5085577a6c37c96df37b777e1b09a5080e61c466
copymary9170 <copymary9170@gmail.com>
2026-07-13 23:48:32 -0400

    Activar cotizaciones por procesos productivos

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from src.inventory_enterprise_loader import activate_inventory_enterprise
 from src.module_bootstrap import activate_module_bootstrap
 from src.print_cost_loader import activate_print_cost_module
 from src.printer_asset_specs import activate_printer_asset_specs
+from src.process_quote_loader import activate_process_quotes
 from src.top_navigation_app import run_app
 
 activate_module_bootstrap()
@@ -11,4 +12,5 @@ activate_printer_asset_specs()
 activate_print_cost_module()
 activate_finishing_modules()
 activate_inventory_enterprise()
+activate_process_quotes()
 run_app()
