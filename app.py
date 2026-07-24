a8e6f6c64e3679815cbfc76a43b221934cc1e5e5
copymary9170 <copymary9170@gmail.com>
2026-07-24 18:56:53 -0400

    Activar integración visible de cadena de suministro

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from src.module_bootstrap import activate_module_bootstrap
 from src.print_cost_loader import activate_print_cost_module
 from src.printer_asset_specs import activate_printer_asset_specs
 from src.startup_restore import restore_session_snapshot_on_startup
+from src.supply_chain_integration import activate_supply_chain_integration
 from src.top_navigation_app import run_app
 
 
@@ -26,6 +27,7 @@ activate_printer_asset_specs()
 activate_print_cost_module()
 activate_finishing_modules()
 activate_inventory_enterprise()
+activate_supply_chain_integration()
 _activate_process_quotes_safely()
 
 # Se ejecuta en cada rerun, pero solo escribe cuando la huella de Configuración
